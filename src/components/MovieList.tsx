2ae547b0758b288149934290c730bbf21fdf627f
Iomannen <maxymdavidov@gmail.com>
2025-03-03 17:03:18 +0300

    Пофиксил стили спиннера и алерта, исправлена типизация ошибки

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -2,6 +2,7 @@ import { FC, useState, useEffect } from "react";
 import style from "./movieList.module.css";
 import { Spin, Alert } from "antd";
 import MovieCard from "./movieCard/MovieCard";
+
 const options = {
   method: "GET",
   headers: {
@@ -10,6 +11,7 @@ const options = {
       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwN2IyNDYzMGY0MjcyZmM2MWQ0YzkxNGIzNTFiNzNhNCIsIm5iZiI6MTc0MDczMTM5MS4wOTEsInN1YiI6IjY3YzE3M2ZmMWYzZjgxYjYwN2EyMzE1ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Gmh5Sci3JPkNbF8Y85ap3r7DGnRpgB_k0J7L_3AuHkg",
   },
 };
+
 interface Error {
   state: boolean;
   message: string | null;
@@ -49,19 +51,29 @@ const MovieList: FC = () => {
       ""
     ) : error.message === "Failed to fetch" ? (
       <Alert
-        className="alert"
+        className={style.alert}
         message="Error"
         description="Failed to fetch. Please try to reload page using VPN"
         type="error"
         showIcon
       />
     ) : (
-      <Alert message="Error" description={error.message} type="error" showIcon className="alert" />
+      <Alert
+        message="Error"
+        description={error.message}
+        type="error"
+        showIcon
+        className={style.alert}
+      />
     );
   };
 
   const showSpinner = () => {
-    return movieList.length === 0 && !error.state ? <Spin size="large" className="alert" /> : "";
+    return movieList.length === 0 && !error.state ? (
+      <Spin size="large" className={style.alert} />
+    ) : (
+      ""
+    );
   };
 
   useEffect(() => {
@@ -92,9 +104,8 @@ const MovieList: FC = () => {
         console.log(data.results);
         setMovies(data.results);
       } catch (e: unknown) {
-        if (e.message === "Failed to fetch") {
-          setError({ state: true, message: e.message });
-        }
+        if (!(e instanceof Error)) return;
+        setError({ state: true, message: e.message });
       }
     };
 
